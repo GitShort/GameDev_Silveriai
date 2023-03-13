e663a65b7e315e667f9484632844830ccbf1601b
Airidas <airidas12111@gmail.com>
2023-03-13 13:04:09 +0200

    Character controller progress

---
+++ Assets/Scripts/FirstPersonCam.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FirstPersonCam : MonoBehaviour
+{
+    [SerializeField] float sensX;
+    [SerializeField] float sensY;
+
+    [SerializeField] Transform orientation;
+    [SerializeField] GameObject cameraObject;
+
+    float xRotation;
+    float yRotation;
+
+    private void Awake()
+    {
+        
+    }
+
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Mouse input
+        float mouseX = Input.GetAxisRaw("Mouse X") * Time.fixedDeltaTime * sensX;
+        float mouseY = Input.GetAxisRaw("Mouse Y") * Time.fixedDeltaTime * sensY;
+
+        yRotation += mouseX;
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90, 90f);
+
+        // Rotate cam and orientation
+        cameraObject.transform.rotation = Quaternion.Euler(xRotation, yRotation, 0);
+        orientation.rotation = Quaternion.Euler(0, yRotation, 0);
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,154 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [Header("Movement")]
+    [SerializeField] float moveSpeed;
+    [SerializeField] Transform orientation;
+    [SerializeField] float groundDrag;
+    [SerializeField] float friction;
+
+    [SerializeField] bool useSprint = true;
+    [SerializeField] float sprintSpeedMultiplier = 1.5f;
+
+    bool isSprinting = false;
+    float originalMoveSpeed;
+
+    [Header("Jumping")]
+    [SerializeField] float jumpForce;
+    [SerializeField] float jumpCooldown;
+    [SerializeField] float airMultiplier;
+    bool readyToJump;
+
+    [SerializeField] float fallMultiplier = 2.5f;
+    [SerializeField] bool useLowJump = true;
+    [SerializeField] float lowJumpMultiplier = 2f;
+    
+
+
+    [Header("Keybinds")]
+    [SerializeField] KeyCode jumpKey = KeyCode.Space;
+    [SerializeField] KeyCode sprintKey = KeyCode.LeftShift;
+
+    [Header("Ground check")]
+    [SerializeField] float playerHeight;
+    [SerializeField] LayerMask whatIsGround;
+    bool grounded;
+
+    Vector3 moveDirection;
+    Rigidbody rb;
+
+    float horizontalInput;
+    float verticalInput;
+    [SerializeField] PhysicMaterial physicMaterial;
+
+    private void Start()
+    {
+        originalMoveSpeed = moveSpeed;
+        rb = GetComponent<Rigidbody>();
+        rb.freezeRotation = true;
+        ResetJump();
+    }
+
+    private void Update()
+    {
+        grounded = Physics.Raycast(transform.position, Vector3.down, playerHeight * 0.5f + 0.2f, whatIsGround);
+
+        MyInput();
+        SpeedControl();
+
+        if (grounded)
+        {
+            physicMaterial.dynamicFriction = friction;
+            rb.drag = groundDrag;
+        }
+        else
+        {
+            physicMaterial.dynamicFriction = 0.01f;
+            rb.drag = 0;
+        }
+
+        if (Input.GetKey(sprintKey) && !isSprinting && grounded)
+        {
+            isSprinting = true;
+            moveSpeed *= sprintSpeedMultiplier;
+        }
+        else if (!Input.GetKey(sprintKey) && isSprinting && grounded)
+        {
+            isSprinting = false;
+            moveSpeed = originalMoveSpeed;
+        }
+
+        //Debug.Log(rb.velocity.magnitude);
+        BetterJump();
+    }
+
+    private void FixedUpdate()
+    {
+        MovePlayer();
+    }
+
+    void MyInput()
+    {
+        horizontalInput = Input.GetAxisRaw("Horizontal");
+        verticalInput = Input.GetAxisRaw("Vertical");
+
+        if (Input.GetKey(jumpKey) && readyToJump && grounded)
+        {
+            readyToJump = false;
+            Jump();
+
+            Invoke(nameof(ResetJump), jumpCooldown);
+        }
+    }
+
+    void MovePlayer()
+    {
+        moveDirection = orientation.forward * verticalInput + orientation.right * horizontalInput;
+
+        // ground
+        if (grounded)
+            rb.AddForce(moveDirection.normalized * moveSpeed * 10f, ForceMode.Force);
+
+        // air
+        else if (!grounded)
+            rb.AddForce(moveDirection.normalized * moveSpeed * 10f * airMultiplier, ForceMode.Force);
+    }
+
+    void SpeedControl()
+    {
+        Vector3 flatVel = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
+        if (flatVel.magnitude > moveSpeed)
+        {
+            Vector3 limitedVel = flatVel.normalized * moveSpeed;
+            rb.velocity = new Vector3(limitedVel.x, rb.velocity.y, limitedVel.z);
+        }
+    }
+
+    void Jump()
+    {
+        rb.velocity = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
+
+        rb.AddForce(transform.up * jumpForce, ForceMode.Impulse);
+    }
+
+    void BetterJump()
+    {
+        if (rb.velocity.y < 0)
+        {
+            rb.velocity += Vector3.up * Physics.gravity.y * (fallMultiplier - 1) * Time.deltaTime;
+        }
+        else if (rb.velocity.y > 0 && !Input.GetKey(jumpKey) && useLowJump)
+        {
+            rb.velocity += Vector3.up * Physics.gravity.y * (lowJumpMultiplier - 1) * Time.deltaTime;
+        }
+    }
+
+    void ResetJump()
+    {
+        Debug.Log("Reset jump");
+        readyToJump = true;
+    }
+}
