daf3549c3c54b64a5a749e52ad0ee8e856350982
Airidas <airidas12111@gmail.com>
2023-03-16 12:55:03 +0200

    Minor shooting adjustments

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -4,8 +4,14 @@ using UnityEngine;
 
 public class Projectile : MonoBehaviour
 {
+    [SerializeField] GameObject destroyParticles;
+
     private void OnTriggerEnter(Collider other)
     {
+        GameObject particles = Instantiate(destroyParticles, transform.position, Quaternion.identity);
+        particles.GetComponent<ParticleSystem>().Play();
+
+        Destroy(particles, 3f);
         Destroy(this.gameObject);
     }
 }
